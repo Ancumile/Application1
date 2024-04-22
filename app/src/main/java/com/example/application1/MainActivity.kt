404afb469b231c6ad37877678764f787125a86cc
AncumileQuluba <128705754+Ancumile@users.noreply.github.com>
2024-04-22 18:49:47 +0200

    First Commit

---
+++ app/src/main/java/com/example/application1/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.application1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var edittextName: EditText
+}
+
